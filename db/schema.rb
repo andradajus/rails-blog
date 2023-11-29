3e5afd98ba1a3b2bee5d76697e4f2305ee03eb60
andradajus <andradajustinmarcus@gmail.com>
2023-11-29 16:54:32 +0800

    Accomplish blog app no design yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_045809) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_085003) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_22_045809) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "username"
+    t.boolean "admin"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
